acb80fe0f6c6b17a3c537b5ce079389b1cbf41d8
Domicio Jose <testedomicio@hotmail.com>
2021-11-15 18:15:41 -0300

    CSS of the Table material and creating a module

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
